767ebd002089d84bb810de75d7aead00d22a84e5
Sanskruti-Gohil <spgohil3108@gmail.com>
2024-10-14 10:52:07 +0530

    Commit 1

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div classname='hero'>
+        <div className="hero-left">
+
+        </div>
+        <div className="hero-right">
+            <h2>NEW ARRIVALS ONLY</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
