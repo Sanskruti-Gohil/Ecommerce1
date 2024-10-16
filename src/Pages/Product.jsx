00a20df66c4575bee7d1f5606bc63a242b9444d4
PranitaBhatt <pranitabhatt00@gmail.com>
2024-10-16 14:09:40 +0530

    n

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react'
-import {ShopContext } from '../Context/ShopContext'
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
 const{all_product}=useContext(ShopContext);
@@ -11,7 +11,7 @@ const product=all_product.find((e)=>e.id === Number (productId))
  return (
     <div>
       <Breadcrum product={product}/>
-      <ProductDisplay product={product}/>
+      <ProductDisplay produc t={product}/>
     </div>
 )
 }
