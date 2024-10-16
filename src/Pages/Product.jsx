7ca9f71868add5141275d8be370a5c63e27b713c
Sanskruti-Gohil <spgohil3108@gmail.com>
2024-10-17 01:04:03 +0530

    commit 10

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -5,15 +5,16 @@ import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
-const{all_product}=useContext(ShopContext);
-const{productId}=useParams();
-const product = all_product.find((e)=>e.id === Number(productId));
- return (
-    <div>
-      <Breadcrum product={product}/>
-      <ProductDisplay product={product}/>
-    </div>
-)
-}
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
 
-export default Product
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
+};
+
+export default Product;
