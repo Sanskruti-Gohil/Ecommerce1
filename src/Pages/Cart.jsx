cb56c46b99a144468808a8d0c042e287f4a9271d
Sanskruti-Gohil <spgohil3108@gmail.com>
2024-10-17 01:23:13 +0530

    c13

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import CartItems from '../Components/CartItems/CartItems'
+// CartItems.js
+import React from 'react';
 
-const Cart = () => {
+const CartItems = ({ all_product }) => {
   return (
     <div>
-      <CartItems/>
+      {/* Ensure all_product exists before mapping */}
+      {all_product && all_product.length > 0 ? (
+        all_product.map((item, index) => (
+          <div key={index}>
+            <h2>{item[0]}</h2> {/* Product name */}
+            <p>{item[1]}</p>   {/* Description */}
+            <p>${item[2]}</p>  {/* Price */}
+          </div>
+        ))
+      ) : (
+        <p>No products available.</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default CartItems;
